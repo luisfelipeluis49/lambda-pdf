9e7e591b5ddf943ecfa701f53243914d157a49be
luisfelipeluis49 <luisfelipeluis49@gmail.com>
2026-01-05 01:35:08 -0300

    removed ignored files from repo

--- _wasm/lambda_pdf.d.ts
+++
@@ -1,6 +0,0 @@
-// @generated minimal typings
-/* tslint:disable */
-/* eslint-disable */
-
-export declare function instantiate(): Promise<typeof import('./lambda_pdf_rs.js')>;
-export default instantiate;
